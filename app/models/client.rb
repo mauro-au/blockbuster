521aac57e1a9a057818b53c7472503479a19b7e7
mauro-au <mauricio.a.developer@gmail.com>
2025-01-03 04:20:08 -0300

    Se agrega validacion para los formularios

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ApplicationRecord
   belongs_to :movie, optional: true
+  validates :name, presence: true
+  validates :age, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   has_many :clients, dependent: :nullify
+  validates :title, presence: true
 end
